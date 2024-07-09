5fd83b16aa3e63b12787f5cd2671d43e40c55e58
sinh <sinhdangvan27@gmail.com>
2024-07-09 11:31:54 +0700

    final RoomType

--- obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+299b9b4006ec811d725f9ff5325ec9fabdbfc3e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523d451b0813cfdac67e371c4483ff82d2acc4ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
