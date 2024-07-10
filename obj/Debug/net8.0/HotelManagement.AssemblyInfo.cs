a94304c145b9bccee86ffe91876c3fe93d2f574b
sinh <sinhdangvan27@gmail.com>
2024-07-10 11:19:05 +0700

    sinh

--- obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+287931e0e01b65acb165636a2d76168024a6d484")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6bd9d62d78889b1a4372bdebffba0348333da8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
